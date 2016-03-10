63a4f19964c363e89351fe07a6639d0677487cc3
Adam-- <andersad@gmail.com>
2016-03-10 12:01:36 -0500

    Basic implementation of Android WebView based browser using Xamarin.Android.

---
+++ XamWebViewBrowser/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace XamWebViewBrowser
+{
+    using Android.Webkit;
+
+    [Activity(Label = "XamWebViewBrowser", MainLauncher = true, Icon = "@drawable/icon", HardwareAccelerated = true)]
+    public class MainActivity : Activity
+    {
+        private EditText editText;
+
+        private WebView webView;
+
+        private WebChromeClient webChromeClient;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            this.SetContentView(Resource.Layout.Main);
+
+            var goButton = FindViewById<Button>(Resource.Id.goButton);
+
+            this.editText = this.FindViewById<EditText>(Resource.Id.urlText);
+
+            this.webChromeClient = new WebChromeClient();
+            this.webView = this.FindViewById<WebView>(Resource.Id.webView);
+            this.webView.Settings.JavaScriptEnabled = true;
+            this.webView.SetWebChromeClient(this.webChromeClient);
+
+            goButton.Click += delegate
+                {
+                    this.webView.LoadUrl(this.editText.Text);
+                };
+        }
+    }
+}
+
